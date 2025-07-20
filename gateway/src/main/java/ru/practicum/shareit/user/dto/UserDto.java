8bbdf0435f4aa33fa75062a9f31ddd24619c7dfb
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-07-20 18:54:53 +0300

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,7 +13,6 @@ public class UserDto {
     Integer id;
     @NotBlank(message = "Имя пользователя не может быть пустым")
     private String name;
-
     @NotBlank(message = "Email не может быть пустым")
     @Email(message = "Некорректный формат email")
     String email;
