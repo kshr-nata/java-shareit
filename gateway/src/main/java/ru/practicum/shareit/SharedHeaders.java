84dc35e7a3dbd58c54136fc1f946aae12658c171
Natalya Abramenko <natalya-gospodinova@yandex.ru>
2025-07-21 21:00:12 +0300

    chekstyle

--- gateway/src/main/java/ru/practicum/shareit/SharedHeaders.java
+++ gateway/src/main/java/ru/practicum/shareit/SharedHeaders.java
@@ -3,5 +3,6 @@ package ru.practicum.shareit;
 public final class SharedHeaders {
     public static final String USER_ID_HEADER = "X-Sharer-User-Id";
 
-    private SharedHeaders() {} // Запрет создания экземпляров
+    private SharedHeaders() {
+    }
 }
